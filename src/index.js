549746430fe6cad06f083ce00a4cd55ae1362d81
zieeco <zieecosam@gmail.com>
2022-02-01 18:20:54 +0100

    Add styling, and update the index.js file

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-import './style.css';
+import './css/reset.css';
+import './css/style.css';
+import j from './assets/jos.jpeg';
+import z from './assets/zieeco.jpeg';
+
+import render from './homeRender';
+
+const icon1 = document.querySelector('.img-icon1');
+const icon2 = document.querySelector('.img-icon2');
+
+icon1.innerHTML += `<img class="jos-ziee" src="${j}" alt="zieeco">`;
+icon2.innerHTML += `<img class="jos-ziee" src="${z}" alt="jos">`;
+
+
+
+render();
+
+
