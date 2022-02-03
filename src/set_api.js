2a19becd689493c9ec07d1081ee58a663ece9876
joskalenda <joskalenda3@gmail.com>
2022-02-03 17:35:33 +0200

    Add footer

--- src/set_api.js
+++ src/set_api.js
@@ -10,35 +10,3 @@ const getTechs = async () => {
 };
 
 export default getTechs;
-
-
-//  set up api for likes and comments
-export default async (id, user, comment) => {
-  fetch(`${involvementApi}${commentsUrl}`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        item_id: id,
-        username: user,
-        comment,
-      }),
-    },
-    );
-  };
-  
-  const ShowComment = (data) => {
-    // const list = document.querySelector('.comment-list');
-    if (!data.length) {
-      comment.innerHTML = 'No comment found !'
-    } else {
-      data.forEach((data) => {
-        date.innerHTML += `${data.creation_date} |`;
-        name.innerHTML +=`${data.username} :`;
-        comment.innerHTML +=`${data.comment}`;
-      // list.innerHTML += `<p><small>${d.creation_date}</small> | <span>${d.username}:</span> ${d.comment}</p>`;
-    });
-  }
-};
